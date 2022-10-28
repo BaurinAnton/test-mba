0837b75ce5f547a47d418b2f31ebdfc9928ec4af
BaurinAnton <baurinanton2013@yandex.ru>
2022-10-28 15:37:30 +0300

    config local db

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         host: env('DATABASE_HOST'),
-        srv: env.bool('DATABASE_SRV', true),
+        srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT'),
         database: env('DATABASE_NAME'),
         username: env('DATABASE_USERNAME'),
@@ -13,7 +13,7 @@ module.exports = ({ env }) => ({
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
